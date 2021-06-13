c8d299aa6ac34e645247f10f4ad4f16ddea1de17
Sang <jdsaeyqi@naver.com>
2021-06-13 22:14:31 +0900

    프로그래머스 내적

---
+++ app/src/main/java/programmers/inner_product.kt
@@ -0,0 +1,14 @@
+package programmers
+
+class Solution1 {
+    fun solution(a: IntArray, b: IntArray): Int {
+        var answer = 0
+
+        for(i in a.indices){
+            answer += a[i] * b[i]
+        }
+
+
+        return answer
+    }
+}
