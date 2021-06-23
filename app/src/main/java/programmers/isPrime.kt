6db83cce076c4df22d7409d4612506910bbb41b5
Sang <jdsaeyqi@naver.com>
2021-06-23 21:20:24 +0900

    프로그래머스 소수

---
+++ app/src/main/java/programmers/isPrime.kt
@@ -0,0 +1,56 @@
+package programmers
+
+class isPrime{
+
+    fun checkPrime(num : Int) : Boolean {
+
+        val sqrt = Math.sqrt(num.toDouble()).toInt()
+
+        if (num == 2) {
+            return true
+        }
+
+        if(num %2 == 0 || num == 1){
+            return false
+        }
+
+        for(i in 3..sqrt){
+            if(num % i == 0){
+                return false
+            }
+        }
+        return true
+    }
+
+    fun eratos(check : BooleanArray){
+
+        check[0] = true
+        check[1] = true
+
+        for(i in 2 until check.size){
+            if(!check[i]){
+                for (j in 2*i until check.size step i){
+                    check[j] = true
+                }
+            }
+        }
+
+        for(i in check.indices){
+            if(!check[i]){
+                print("$i ")
+            }
+        }
+
+    }
+
+}
+
+fun main(){
+    val s = isPrime().checkPrime(12)
+    println(s)
+
+    val check = BooleanArray(20)
+    isPrime().eratos(check)
+
+
+}
