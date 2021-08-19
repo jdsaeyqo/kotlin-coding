6e456a62f024ee5c72c86211f8df2f730c86714e
Sang <jdsaeyqi@naver.com>
2021-08-19 15:45:21 +0900

    백준 수들의 합 5

---
+++ app/src/main/java/boj/twopointer/BOJ2018.kt
@@ -0,0 +1,42 @@
+package boj.twopointer
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class BOJ2018 {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    fun main() {
+
+        val num = kot.br.readLine().toInt()
+
+        if (num == 1) {
+            println(1)
+            return
+        }
+        var ans = 0
+        var s = 1
+        var e = 2
+        var sum = s + e
+
+        while (!(s == e && e == num)) {
+            if (sum < num) {
+                e++
+                sum += e
+            } else if (sum == num) {
+                ans++
+                sum -= s
+                s++
+            } else {
+                sum -= s
+                s++
+
+            }
+        }
+
+        println(ans + 1)
+
+
+    }
+
+
+}
