11265144133d2fa922b031786146d5cb4e57c48e
Sang <jdsaeyqi@naver.com>
2021-09-13 21:13:59 +0900

    프로그래머스 위클리 6주차 해결

--- app/src/main/java/programmers/simulation/Weekly6th.kt
+++ app/src/main/java/programmers/simulation/Weekly6th.kt
@@ -2,32 +2,39 @@ package programmers.simulation
 
 import kot.Boxer
 
-// 테스트케이스 5번 해결 안됨
 class Weekly6th {
     fun solution(weights: IntArray, head2head: Array<String>): IntArray {
 
         val boxerList = arrayListOf<Boxer>()
         val n = weights.size
-
+        var noMatch = ""
+        for(i in 0 until n){
+            noMatch+="N"
+        }
         for (i in 0 until n) {
             val boxer = Boxer(num = i + 1, weight = weights[i])
-            for (j in 0 until n) {
-                if(i!=j){
 
-                    if(head2head[i][j] !='N'){
-                        boxer.match++
-                    }
-                    if (head2head[i][j] == 'W') {
-                        boxer.win++
-                        if (weights[i] < weights[j]) {
-                            boxer.winToBig++
+            if(head2head[i] == noMatch){
+                boxer.rate = 0.0
+            }else{
+                for (j in 0 until n) {
+                    if(i!=j){
+
+                        if(head2head[i][j] !='N'){
+                            boxer.match++
+                        }
+                        if (head2head[i][j] == 'W') {
+                            boxer.win++
+                            if (weights[i] < weights[j]) {
+                                boxer.winToBig++
+                            }
                         }
                     }
-                }
 
+                }
+                boxer.rate = boxer.win / boxer.match.toDouble() * 100
             }
 
-            boxer.rate = boxer.win / boxer.match.toDouble() * 100
             boxerList.add(boxer)
 
         }
