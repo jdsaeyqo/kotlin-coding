dc04b00ab95f32625971d3b2076bbf9536527f41
Sang <jdsaeyqi@naver.com>
2021-09-18 16:19:30 +0900

    프로그래머스 위클리 7주차 새로운 접근 필요

--- app/src/main/java/programmers/simulation/Weekly7th.kt
+++ app/src/main/java/programmers/simulation/Weekly7th.kt
@@ -2,25 +2,11 @@ package programmers.simulation
 
 class Weekly7th {
 
-    //7주차 해결중
+   //7주차 새로운 접근 필요..
     fun solution(enter: IntArray, leave: IntArray): IntArray {
 
         val answer = IntArray(enter.size)
 
-        val enterList = enter.toMutableList()
-
-        leave.forEach { num ->
-            val index = enterList.indexOf(num)
-
-            if(index > 0){
-                for(i in 0 until index){
-                    answer[num]++
-                    answer[enterList[0]]++
-
-
-                }
-
-        } }
 
 
         return answer
