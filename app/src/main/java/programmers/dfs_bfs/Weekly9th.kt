866dab72e5d3a86a632363d147af6f89c2183d75
Sang <jdsaeyqi@naver.com>
2021-10-06 21:24:34 +0900

    프로그래머스 위클리 9주차

---
+++ app/src/main/java/programmers/dfs_bfs/Weekly9th.kt
@@ -0,0 +1,15 @@
+package programmers.dfs_bfs
+
+class Weekly9th {
+    fun solution(n: Int, wires: Array<IntArray>): Int {
+        var answer: Int = -1
+
+        /* 2중 배열 입력
+           각각 1씩 연결 되어있는 걸 하나씩 없애가며
+           두개 그룹으로 나눔
+           각 그룹의 차이의 절대값이 가장 작을 때 출력
+        */
+
+        return answer
+    }
+}
