15f9128c6d67708b167dafafb41ab20bd9112568
Sang <jdsaeyqi@naver.com>
2021-10-08 09:57:04 +0900

    프로그래머스 위클리 9주차

--- app/src/main/java/programmers/dfs_bfs/Weekly9th.kt
+++ app/src/main/java/programmers/dfs_bfs/Weekly9th.kt
@@ -1,15 +1,64 @@
 package programmers.dfs_bfs
 
 class Weekly9th {
+    var cnt = 0
+    var ans = 100
     fun solution(n: Int, wires: Array<IntArray>): Int {
-        var answer: Int = -1
 
-        /* 2중 배열 입력
-           각각 1씩 연결 되어있는 걸 하나씩 없애가며
-           두개 그룹으로 나눔
-           각 그룹의 차이의 절대값이 가장 작을 때 출력
-        */
+        val map = Array(n+1){IntArray(n+1)}
 
-        return answer
+        wires.forEach{
+            map[it[0]][it[1]] = 1
+            map[it[1]][it[0]] = 1
+
+        }
+
+        wires.forEach{
+
+            val copyMap = copyMap(n,map)
+            val x = it[0]
+            val y = it[1]
+            copyMap[x][y] = 0
+            copyMap[y][x] = 0
+            val visit = BooleanArray(n+1)
+            val result = arrayListOf<Int>()
+
+            for(i in 1..n){
+                if(!visit[i]){
+                    dfs(copyMap,visit,i,n)
+                    result.add(cnt)
+                    cnt = 0
+                }
+
+
+            }
+
+            ans = Math.min(ans,Math.abs(result[0]-result[1]))
+
+        }
+
+        return ans
+    }
+
+    fun dfs(copyMap : Array<IntArray>, visit : BooleanArray, i : Int,n : Int) {
+        cnt++
+        visit[i] = true
+        for(j in 1..n){
+            if( i!=j && !visit[j] && copyMap[i][j] == 1){
+                dfs(copyMap , visit,j,n)
+            }
+        }
+
+    }
+
+    fun copyMap(n : Int,map : Array<IntArray>) : Array<IntArray> {
+        val copy = Array(n+1){IntArray(n+1)}
+
+        for(i in 1..n){
+            for(j in 1..n){
+                copy[i][j] = map[i][j]
+            }
+        }
+        return copy
     }
 }
